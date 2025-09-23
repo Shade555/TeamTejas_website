7286cf2c6aefd883aba55199077be0579fc90ffb
Anzila <anzilaakhan09@gmail.com>
2025-09-23 22:46:59 +0530

    Initialized navbar

---
+++ ts/app/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      style={{
+        width: "100%",
+        padding: "1rem",
+        background: "#222",
+        color: "#fff",
+        textAlign: "center",
+      }}
+    >
+      <h1>Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
